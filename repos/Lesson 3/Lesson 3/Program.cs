8f3fb7a60d3572cbad3ab45f852d7a46e720f740
phisio76 <83365774+phisio76@users.noreply.github.com>
2021-05-08 22:41:54 +0300

    Add files via upload

---
+++ repos/Lesson 3/Lesson 3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System; // а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.
+
+struct Complex
+{
+    public double im;
+    public double re;
+    //  в C# в структурах могут храниться также действия над данными
+    public Complex Plus(Complex x)
+    {
+        Complex y;
+        y.im = im + x.im;
+        y.re = re + x.re;
+        return y;
+    }
+    //  Пример произведения двух комплексных чисел
+    public Complex Multi(Complex x)
+    {
+        Complex y;
+        y.im = re * x.im + im * x.re;
+        y.re = re * x.re - im * x.im;
+        return y;
+    }
+    //  Дописать структуру Complex, добавив метод вычитания комплексных чисел.
+    public Complex Minus(Complex x)
+    {
+        Complex y;
+        y.im = im - x.im;
+        y.re = re - x.re;
+        return y;
+    }
+    public string ToString()
+    {
+        return re + "+" + im + "i";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число 1 (сложение), 2 (умножение) или 3 (вычитание): ");
+        Complex complex1;
+        complex1.re = 3;
+        complex1.im = 3;
+
+        Complex complex2;
+        complex2.re = 2;
+        complex2.im = 2;
+        int caseSwitch = int.Parse(Console.ReadLine()); // в) Добавить диалог с использованием switch демонстрирующий работу класса.
+        switch (caseSwitch)
+        {
+            case 1:
+                Complex result = complex1.Plus(complex2);
+                Console.WriteLine(result.ToString());
+                goto case 4;
+            case 2:
+                result = complex1.Multi(complex2);
+                Console.WriteLine(result.ToString());
+                goto case 4;
+            case 3:
+                result = complex1.Minus(complex2);
+                Console.WriteLine(result.ToString());
+                goto case 4;
+            case 4:
+                Console.WriteLine("Демонстрация работы структуры:");  // Продемонстрировать работу структуры.
+                Complex result1 = complex1.Plus(complex2);
+                Console.WriteLine($"Результом операции сложения чисел: {complex1.ToString()}  и {complex2.ToString()}  является {result1.ToString()}");
+                Complex result2 = complex1.Multi(complex2);
+                Console.WriteLine($"Результом операции умножения чисел: {complex1.ToString()}  и {complex2.ToString()}  является {result2.ToString()}");
+                Complex result3 = complex1.Minus(complex2);
+                Console.WriteLine($"Результом операции вычитания чисел: {complex1.ToString()}  и {complex2.ToString()}  является {result3.ToString()}");
+
+                break;
+        }
+    }
+}
---
+++ repos/Lesson 3/Lesson3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; // С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse.
+
+namespace Lesson_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int num = 0;
+            Console.WriteLine("Вводите различные числа и нажимайте enter:");
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num > 0 && num % 2 == 1)
+                    sum += num;
+
+            } while (num != 0);
+
+            Console.WriteLine($"Сумма нечётных чисел равна {sum}");
+        }
+    }
+}
+
